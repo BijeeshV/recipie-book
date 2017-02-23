6f1a66e28bdf3bc9072585b86b2432d144404d3d
Bijeesh <bijeesh.v@firmusoft.co.in>
2017-02-23 14:22:34 +0530

    Recipe Add

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,
+  FormControl,
+  Validators,
+  FormArray
+} from '@angular/forms'
 
 @Component({
   selector: 'rb-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent implements OnInit {
+/*
+class ingredients{
+  constructor(public name:FormControl,public amount:number){}
+}
+*/
 
-  constructor() { }
+export class RecipeEditComponent implements OnInit {
+myForm:FormGroup;
+
+  constructor() {
+    this.myForm=new FormGroup({
+      'recipe':new FormGroup({
+        'name':new FormControl('',Validators.required),
+      'description':new FormControl('',Validators.required),
+      'imagePath':new FormControl('',Validators.required)
+      }),
+      'ingredients':new   FormArray([
+         new FormGroup({
+            'name':new FormControl('',Validators.required),
+            'amount':new FormControl('',Validators.required)
+         })       
+      ])
+    });
+   }
 
   ngOnInit() {
   }
-
+  onAddIngredient()
+  {
+    (<FormArray>this.myForm.controls['ingredients']).push(new FormGroup({
+      'name':new FormControl('',Validators.required),
+      'amount':new FormControl('',Validators.required)
+    }))
+  }
+  onSubmit(){
+    console.log(this.myForm);
+  }
 }
